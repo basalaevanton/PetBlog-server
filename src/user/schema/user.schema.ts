a236d5fb0fa7856686cdb36ec32203f014874390
basalaevanton <74416144+basalaevanton@users.noreply.github.com>
2022-01-17 13:25:16 +0300

    doing it live

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -1,33 +1,33 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import * as mongoose from 'mongoose';
-import { Post } from 'src/post/schemas/post.schema';
-import { Comment } from 'src/comment/schema/comment.schema';
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { Document } from 'mongoose';
+// import * as mongoose from 'mongoose';
+// import { Post } from 'src/post/schemas/post.schema';
+// import { Comment } from 'src/comment/schema/comment.schema';
 
-export type UserDocument = User & Document;
+// export type UserDocument = User & Document;
 
-@Schema()
-export class User {
-  @Prop()
-  firstName: string;
+// @Schema()
+// export class User {
+//   @Prop()
+//   firstName: string;
 
-  @Prop()
-  lastName: string;
+//   @Prop()
+//   lastName: string;
 
-  @Prop()
-  email: string;
+//   @Prop()
+//   email: string;
 
-  @Prop()
-  password: string;
+//   @Prop()
+//   password: string;
 
-  @Prop()
-  role: string;
+//   @Prop()
+//   role: string;
 
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }] })
-  comments: Comment[];
+//   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }] })
+//   comments: Comment[];
 
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }] })
-  posts: Post[];
-}
+//   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }] })
+//   posts: Post[];
+// }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+// export const UserSchema = SchemaFactory.createForClass(User);
